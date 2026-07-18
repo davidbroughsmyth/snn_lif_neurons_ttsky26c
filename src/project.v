8a7a4d8dc6563a3f025b1eacb31bb7991d780a09
David Brough-Smyth <davidb@javahouse.com.au>
2026-07-18 17:57:40 +1000

    Rename module from tt_snn_lif_neuron to tt_um_snn_lif_neuron

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_snn_lif_neuron (
+module tt_um_snn_lif_neuron (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
