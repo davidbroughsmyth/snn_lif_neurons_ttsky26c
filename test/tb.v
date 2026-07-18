d6215e2f694804190c7d112559d0e556f52c31ba
David Brough-Smyth <davidb@javahouse.com.au>
2026-07-18 17:57:56 +1000

    Rename module from tt_um_example to tt_um_snn_lif_neuron

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_snn_lif_neuron user_project (
+  tt_um_snn_lif_neuron user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
